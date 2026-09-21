f5579c989d13be19124b66e144de0d982296c0d4
gabrielrozendo-max <gabriel.rozendo@aluno.impacta.edu.br>
2026-09-21 19:45:47 -0300

    Add files via upload

---
+++ exercicio33.py
@@ -0,0 +1,18 @@
+dia = int(input("Número: "))
+
+if dia == 1:
+    print("SEGUNDA-FEIRA")
+elif dia == 2:
+    print("TERÇA-FEIRA")
+elif dia == 3:
+    print("QUARTA-FEIRA")
+elif dia == 4:
+    print("QUINTA-FEIRA")
+elif dia == 5:
+    print("SEXTA-FEIRA")
+elif dia == 6:
+    print("SÁBADO")
+elif dia == 7:
+    print("DOMINGO")
+else:
+    print("OPÇÃO INVÁLIDA")
